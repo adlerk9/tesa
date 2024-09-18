b445f2b541671e2edea23e91d17c1167d46b32bd
adlerk9 <upswing.cz@gmail.com>
2024-09-18 10:34:44 -0300

    testes

--- vaipracasa/config.py
+++ vaipracasa/config.py
@@ -2,5 +2,6 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'supersecretkey'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:senha1234@localhost/Adler'
     SQLALCHEMY_DATABASE_URI = 'postgresql://adler_user:o7prblu8C1SCunm3jTvHlNVUUMPeFzFI@dpg-crl0b7ggph6c73anhhr0-a.oregon-postgres.render.com/adler'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
