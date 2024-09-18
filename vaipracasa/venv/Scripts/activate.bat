972d2d9a99e2dafa9001e213eac6e836555b2809
adlerk9 <upswing.cz@gmail.com>
2024-09-17 21:31:15 -0300

    concluido

--- vaipracasa/venv/Scripts/activate.bat
+++ vaipracasa/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\upswi\OneDrive\Desktop\vaipracasa\venv
+set VIRTUAL_ENV=C:\Users\upswi\OneDrive\Desktop\tesagit\vaipracasa\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
